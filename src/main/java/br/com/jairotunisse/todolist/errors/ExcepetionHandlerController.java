f9646d9dcdd90b263a37d9a5c21c5a02519ba8f6
Jairotsb <jairo_tunisse@outlook.com>
2023-10-16 13:27:23 -0300

    feat: base project finished :tada:

---
+++ src/main/java/br/com/jairotunisse/todolist/errors/ExcepetionHandlerController.java
@@ -0,0 +1,18 @@
+package br.com.jairotunisse.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExcepetionHandlerController {
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableExceptionEntity(HttpMessageNotReadableException error) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error.getMostSpecificCause().getMessage());
+    }
+}
